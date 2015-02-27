5e9b25876997cc2f2987ea23cdc5071653ebb842
Blake Goodwin <bag3cn@virginia.edu>
2015-02-27 16:03:31 -0500

    Remove omniauth.rb keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, '941e67ac3eea40bfaeed052626a07c4a', '70083348e3d74733b08a016bfdff7c22', scope: 'playlist-read-private user-read-private user-read-email playlist-modify playlist-modify-private'
+  provider :spotify, '941e67ac3eea40bfaeed052626a07c4a', '######################', scope: 'playlist-read-private user-read-private user-read-email playlist-modify playlist-modify-private'
 end
